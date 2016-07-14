2beaabebe41291f27b1d12774c96aadd6fdd7291
John Tatum <johnny-b-goode@users.noreply.github.com>
2016-07-14 00:54:59 +0000

    Stubbed out methods for Plugin and Store interfaces.

--- src/net/scientifichooliganism/testlinkplugin/TestLinkPlugin.java
+++ src/net/scientifichooliganism/testlinkplugin/TestLinkPlugin.java
@@ -1,9 +1,12 @@
 package net.scientifichooliganism.testlinkplugin;
 
+import java.util.Collection;
+import java.util.Vector;
+
 import net.scientifichooliganism.javaplug.interfaces.Plugin;
 import net.scientifichooliganism.javaplug.interfaces.Store;
 
-public class TestLinkPlugin {
+public class TestLinkPlugin implements Plugin, Store {
 	private static TestLinkPlugin instance;
 
 	private TestLinkPlugin(){
@@ -16,5 +19,44 @@ public class TestLinkPlugin {
 		}
 
 		return instance;
+	}
+
+	public String[][] getActions() {
+		throw new RuntimeException("TestLinkPlugin.getActions() called");
+	}
+
+	public static boolean isStore() {
+		return true;
+	}
+
+	public void addResource (Object resource) throws IllegalArgumentException {
+		throw new RuntimeException("TestLinkPlugin.() called");
+	}
+
+	public Collection getResources () throws IllegalArgumentException {
+		throw new RuntimeException("TestLinkPlugin.() called");
+	}
+
+	public void removeResource (Object resource) throws IllegalArgumentException {
+		throw new RuntimeException("TestLinkPlugin.() called");
+	}
+
+	public void persist (Object in) throws IllegalArgumentException {
+		throw new RuntimeException("TestLinkPlugin.() called");
+	}
+
+	public Collection query (String query) throws IllegalArgumentException {
+		System.out.println("Yo!");
+		return new Vector();
+	}
+
+	public static void main (String [] args) {
+		try {
+			TestLinkPlugin tlp = TestLinkPlugin.getInstance();
+			tlp.query("Release");
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 }
